34d89c5c8d29e6a5e252665b7b42507c3111b1a5
Sathishkumar Selvaraj <sjksathishkumar@gmail.com>
2022-09-28 16:25:14 +0530

    nginx changes

--- install-wp.sh
+++ install-wp.sh
@@ -3,18 +3,18 @@
 # Install WordPress on a Ubuntu 13+ VPS
 #
 
-# run as root 
+# run as root
 if [[ $UID != 0 ]]; then
-    echo "Please run this script with sudo:"
-    echo "sudo $0 $*"
-    exit 1
+	echo "Please run this script with sudo:"
+	echo "sudo $0 $*"
+	exit 1
 fi
 
 # set colors
-green=`tput setaf 2`
-red=`tput setaf 1`
-normal=`tput sgr0`
-bold=`tput bold`
+green=$(tput setaf 2)
+red=$(tput setaf 1)
+normal=$(tput sgr0)
+bold=$(tput bold)
 
 # start
 echo "${normal}${bold}UBUNTU SITE INSTALLER${normal}"
@@ -28,7 +28,7 @@ if [ $wpFiles == "y" ]; then
 	read -p "Database name: " dbname
 	read -p "Database username: " dbuser
 
-	# If you are going to use root ask about it	
+	# If you are going to use root ask about it
 	if [ $dbuser == 'root' ]; then
 		read -p "${red}root is not recommended. Use it (y/n)?${normal} " useroot
 
@@ -74,7 +74,7 @@ if [ $wpFiles == "y" ]; then
 		# fork of https://drupal.org/node/1681250
 		echo "Checking whether the $dbuser exists and has privileges"
 
-		user_exists=`mysql -u root -p$rootpass -e "SELECT user FROM mysql.user WHERE user = '$dbuser'" | wc -c`
+		user_exists=$(mysql -u root -p$rootpass -e "SELECT user FROM mysql.user WHERE user = '$dbuser'" | wc -c)
 		if [ $user_exists = 0 ]; then
 			echo "CREATE USER '$dbuser'@'localhost' IDENTIFIED BY '$userpass';" | mysql -u root -p$rootpass
 			echo "${green}New MySQL user ($dbuser) was successfully created${normal}"
@@ -82,12 +82,12 @@ if [ $wpFiles == "y" ]; then
 			echo "${red}This MySQL user ($dbuser) already exists${normal}"
 		fi
 
-		user_has_privilage=`mysql -u root -p$rootpass -e "SELECT User FROM mysql.db WHERE db = '$dbname' AND user = '$dbuser'" | wc -c`
+		user_has_privilage=$(mysql -u root -p$rootpass -e "SELECT User FROM mysql.db WHERE db = '$dbname' AND user = '$dbuser'" | wc -c)
 		if [ $user_has_privilage = 0 ]; then
 			echo "GRANT ALL PRIVILEGES ON $dbname.* TO '$dbuser'@'localhost';" | mysql -u root -p$rootpass
 			echo "FLUSH PRIVILEGES;" | mysql -u root -p$rootpass
 			echo "${green}Add privilages for user ($dbuser) to DB $dbname${normal}"
-		else 
+		else
 			echo "${red}User ($dbuser) already has privilages to DB $dbname${normal}"
 		fi
 
@@ -127,75 +127,94 @@ else
 	echo "Skipping WordPress install."
 fi
 
-# Create Apache virtual host
-read -p "Do you want to install Apache vhost (y/n)? " apacheFiles
+# Create Nginx virtual host
+read -p "Do you want to install Nginx vhost (y/n)? " nginxFiles
 
-if [ $apacheFiles == "y" ]; then
+if [ $nginxFiles == "y" ]; then
 
-	if [ -f /etc/apache2/sites-available/$wpURL ]; then
-	    echo "${red}This site already has a vhost file.${normal}"
+	if [ -f /etc/nginx/sites-available/$wpURL ]; then
+		echo "${red}This site already has a vhost file.${normal}"
 	else
 
-	echo "
-<VirtualHost *:80>
-        ServerAdmin webmaster@localhost
-        ServerName $wpURL
-        ServerAlias www.$wpURL
+		echo "
+	server {
 
-        DocumentRoot /var/www/$wpURL
-        <Directory />
-                Options MultiViews +FollowSymLinks
-				AllowOverride All
-				Order allow,deny
-				Allow from All
-        </Directory>
-        <Directory /var/www/>
-                Options FollowSymLinks MultiViews
-                AllowOverride All
-                Order allow,deny
-                Allow from all
-        </Directory>
+        listen 80;
+        listen [::]:80;
+        
+        root /var/www/$wpURL;
 
-        ScriptAlias /cgi-bin/ /usr/lib/cgi-bin/
-        <Directory /usr/lib/cgi-bin>
-                AllowOverride None
-                Options +ExecCGI -MultiViews +SymLinksIfOwnerMatch
-                Order allow,deny
-                Allow from all
-        </Directory>
+        # Add index.php to the list if you are using PHP
+        index index.php index.html index.htm index.nginx-debian.html;
 
-        ErrorLog ${APACHE_LOG_DIR}/error.log
+        server_name $wpURL www.$wpURL;
 
-        # Possible values include: debug, info, notice, warn, error, crit,
-        # alert, emerg.
-        LogLevel warn
+        location = /favicon.ico { log_not_found off; access_log off; }
+        location = /robots.txt { log_not_found off; access_log off; allow all; }
+        location ~* \.(css|gif|ico|jpeg|jpg|js|png)$ {
+                expires max;
+                log_not_found off;
+        }
 
-        CustomLog ${APACHE_LOG_DIR}/access.log combined
-</VirtualHost>
-" > /etc/apache2/sites-available/$wpURL
+        location ~ ^/wp-content/uploads/edd/(.*?)\.zip$ { rewrite / permanent; }
 
-	if [ -f /etc/apache2/sites-available/$wpURL ]; then
-		echo "${green}Apache vhost file created${normal}"
-	else
-		echo "${red}Apache vhost failed to install${normal}"
+        location / {
+                try_files \$uri \$uri/ /index.php\$is_args\$args;
+        }
+
+        location ~ ([^/]*)sitemap(.*).x(m|s)l$ {
+          ## this rewrites sitemap.xml to /sitemap_index.xml
+          rewrite ^/sitemap.xml$ /sitemap_index.xml permanent;
+          ## this makes the XML sitemaps work
+          rewrite ^/([a-z]+)?-?sitemap.xsl$ /index.php?yoast-sitemap-xsl=\$1 last;
+          rewrite ^/sitemap_index.xml$ /index.php?sitemap=1 last;
+          rewrite ^/([^/]+?)-sitemap([0-9]+)?.xml$ /index.php?sitemap=\$1&sitemap_n=\$2 last;
+          ## The following lines are optional for the premium extensions
+          ## News SEO
+          rewrite ^/news-sitemap.xml$ /index.php?sitemap=wpseo_news last;
+          ## Local SEO
+          rewrite ^/locations.kml$ /index.php?sitemap=wpseo_local_kml last;
+          rewrite ^/geo-sitemap.xml$ /index.php?sitemap=wpseo_local last;
+          ## Video SEO
+          rewrite ^/video-sitemap.xsl$ /index.php?yoast-sitemap-xsl=video last;
+        }
+
+        location ~ \.php$ {
+                include snippets/fastcgi-php.conf;
+                fastcgi_pass unix:/run/php/php7.4-fpm.sock;
+        }
+}" >/etc/nginx/sites-available/$wpURL
+
+		if [ -f /etc/nginx/sites-available/$wpURL ]; then
+			echo "${green}Nginx vhost file created${normal}"
+		else
+			echo "${red}Nginx vhost failed to install${normal}"
+		fi
+
 	fi
 
-fi
+	# Enable the site
+	sudo ln -s /etc/nginx/sites-available/$wpURL /etc/nginx/sites-enabled/
+	sudo systemctl restart nginx
 
-# Enable the site
-a2ensite $wpURL
-service apache2 reload
+	# Install ssl certificate
 
-curlText=`curl --user-agent "fogent" --silent "http://$wpURL/wp-admin/install.php" | grep -o -m 1 "Welcome to the famous five minute WordPress installation process" | wc -c`
+	read -p "Do you want to install SSL Certificate (y/n)? " sslInstall
 
-# http://www.cyberciti.biz/faq/how-to-find-out-the-ip-address-assigned-to-eth0-and-display-ip-only/
-yourip=`/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
+	if [ $sslInstall == "y" ]; then
+		sudo certbot --nginx -d $wpURL -d www.$wpURL
+	fi
 
-if [ $curlText == '65' ]; then
-  echo "${green}Go to http://$wpURL and finish install.${normal}";
-else
-  echo "${green}Go to http://$yourip/$wpURL and config your DNS after.${normal}";
-fi
+	curlText=$(curl --user-agent "fogent" --silent "http://$wpURL/wp-admin/install.php" | grep -o -m 1 "Welcome to the famous five minute WordPress installation process" | wc -c)
+
+	# http://www.cyberciti.biz/faq/how-to-find-out-the-ip-address-assigned-to-eth0-and-display-ip-only/
+	yourip=$(/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
+
+	if [ $curlText == '65' ]; then
+		echo "${green}Go to http://$wpURL and finish install.${normal}"
+	else
+		echo "${green}Go to http://$yourip/$wpURL and config your DNS after.${normal}"
+	fi
 
 else
 	echo "Skipping Apache site install."
